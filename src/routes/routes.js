4d7e77bab296835ee9a07752abd09bdf4ece6914
ChrisCedi <christian.cedillo.palacios@outlook.com>
2023-04-25 13:02:42 -0600

    feature: second config, components, navigation and views

---
+++ src/routes/routes.js
@@ -0,0 +1,22 @@
+import { Home } from "../views/Home/Home";
+import { Login } from "../views/Login/Login";
+import { BaseLayout } from "../layouts/BaseLayout";
+
+const routes = [
+  {
+    path: "/login",
+    layout: BaseLayout,
+    component: Login,
+    exact: true,
+    routeMessage: "Home",
+  },
+  {
+    path: "/not-found",
+    layout: BaseLayout,
+    component: Home,
+    exact: true,
+    routeMessage: "Not found",
+  },
+];
+
+export { routes };
