c94f9de1ef5e61f7cb93afd4bd0929d6b8a8dfed
ChrisCedi <christian.cedillo.palacios@outlook.com>
2023-04-26 01:42:41 -0600

    feature: header and drawer responsive

---
+++ src/components/Header/options.js
@@ -0,0 +1,23 @@
+import HomeIcon from "@material-ui/icons/Home";
+import EmailIcon from "@material-ui/icons/Email";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+
+export const menuOptions = [
+  {
+    title: "Inicio",
+    icon: HomeIcon,
+  },
+  {
+    title: "Mensajería",
+    icon: EmailIcon,
+  },
+  {
+    title: "Administración",
+    icon: AccountCircleIcon,
+  },
+  {
+    title: "Acreditados",
+    icon: FavoriteIcon,
+  },
+];
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,21 +1,22 @@
 import { Home } from "../views/Home/Home";
 import { Login } from "../views/Login/Login";
+import { FullScreenLayout } from "../layouts/FullScreenLayout";
 import { BaseLayout } from "../layouts/BaseLayout";
 
 const routes = [
   {
     path: "/login",
-    layout: BaseLayout,
+    layout: FullScreenLayout,
     component: Login,
     exact: true,
-    routeMessage: "Home",
+    routeMessage: "Login",
   },
   {
     path: "/",
     layout: BaseLayout,
     component: Home,
     exact: true,
-    routeMessage: "Not found",
+    routeMessage: "Home",
   },
 ];
 
